5edab88ed37ea5f4a155fd33003fb710584ac1ce
Shafirara <53251973+Shafirara@users.noreply.github.com>
2019-07-24 15:26:00 +0700

    ShafiraRahma

---
+++ ShafiraRahma.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+ 	public static void main(String args[])
+ 	{ 
+ 		for (int k=1;k<=24;k++){
+ 	    for (int j=1;j<=k;j++){
+     		System.out.print("*");
+     	}
+ 		   System.out.println();
+
+}
+}
+}
