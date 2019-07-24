899a58bf514b9e2379e2b540cf77942ad4ab65f2
oktavia1818 <53261691+oktavia1818@users.noreply.github.com>
2019-07-24 18:57:07 +0700

    Dewi oktavia

---
+++ tugas.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+class Dcoder
+{
+    public static void main(String args[])
+    {
+      for (int a=1;a<=7;a++){
+      for (int h=1;h<=a;h++){
+        System.out.print("*");
+}
+        System.out.println();
+}
+     }
+}
