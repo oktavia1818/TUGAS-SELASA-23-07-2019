a534b2cb5ff1fdc19d60c4bf2d50e67bcc0c0e6c
immassintion <53252314+immassintion@users.noreply.github.com>
2019-07-24 15:34:01 +0700

    M.Immas Sintion

---
+++ tion.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+ 	public static void main(String args[])
+ 	{
+ 		for (int t=1;t<=17;t++){
+ 		for (int u=17;u>=t;u--){
+ 			System.out.print("*");
+ 		}
+ 		    System.out.println();	
+ 		}
+ 	}
+      
+ }
